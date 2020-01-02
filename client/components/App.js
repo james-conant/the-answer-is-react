a84c8014c82bed7ee1d1f86408b8b33c7caecf46
james-conant <jconant1993@gmail.com>
2020-01-02 11:51:21 -0700

    first

---
+++ client/components/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { categories } from '../../testdata';
+import Gameboard from './Gameboard.js';
+import Scoreboard from './Scoreboard.js';
+import Response from './Response.js';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: categories,
+      currentQuestion: {},
+      answeredQuestions: [],
+      score: 0
+    };
+  }
+  componentDidMount() {
+    // Getting data from an external API
+    //1. A query to /api/categories to get a set of categories
+    //2. A set of queries afterwards to /api/category at each category id to get clues for that category
+  }
+  render() {
+    return (
+      <div id={'app'}>
+        What is Reactor 2?
+        <Gameboard results={this.state.results} currentQuestion={this.state.currentQuestion} answeredQuestions={this.state.answeredQuestions}/>
+        <Scoreboard score={this.state.score}/>
+        <Response />
+
+      </div>
+    );
+  }
+}
